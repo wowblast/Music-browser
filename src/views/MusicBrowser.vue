133bb228f89bd465eb8f029045fcf229c2069eed
wowblast <jfrt142012@hotmail.com>
2020-07-05 18:23:29 -0400

    search with input added

--- src/views/MusicBrowser.vue
+++ src/views/MusicBrowser.vue
@@ -8,7 +8,7 @@
         <form action>
           <div class="form-group">
             <label for="inputMusic" class="control-label">Enter music name</label>
-            <input class="form-control" id="inputMusic" />
+            <input class="form-control" id="inputMusic" v-model=searchInput />
           </div>
           <button type="button" class="btn btn-primary" v-on:click="search()">Search</button>
         </form>
@@ -27,21 +27,26 @@
 <script>
 import MusicCard from '../components/MusicCard.vue'
 import axios from 'axios'
+import config from '../config.json'
 
 export default {
   data () {
     return {
       title: 'Music  Browser',
-      musicList: [{ data: 'd' }]
+      musicList: [],
+      searchInput: ''
     }
   },
   components: {
     MusicCard
   },
   methods: {
-    search (inputMusic) {
+    search () {
+      console.log(this.searchInput)
+      console.log(config.iTuneHttpLink)
+      this.searchInput = this.searchInput.replace(' ', '+')
       axios
-        .get('https://itunes.apple.com/search?term=jack+johnson')
+        .get(config.iTuneHttpLink + this.searchInput)
         .then(result => {
           console.log(result.data.results)
           this.musicList = result.data.results
